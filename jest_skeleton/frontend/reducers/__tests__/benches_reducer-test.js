67ad37ee41d8a8540db353a04dc58c1d0e73b4c6
tjshiu <tiffany.shiu.2@gmail.com>
2018-05-10 16:16:05 -0700

    added benches reducer tests

--- jest_skeleton/frontend/reducers/__tests__/benches_reducer-test.js
+++ jest_skeleton/frontend/reducers/__tests__/benches_reducer-test.js
@@ -23,3 +23,29 @@ NOTE: Our frontend state shape looks like this:
   ...
 }
 */
+
+describe("BenchesReducer", () => {
+  test("should return the initial state", () => {
+    expect(BenchesReducer(undefined, {})).toEqual({});
+  });
+
+  test("should handle RECEIVE_BENCHES", () => {
+    let action = {
+      type: BenchActions.RECEIVE_BENCHES,
+      benches: testBenches
+    };
+
+    expect(BenchesReducer({}, action)).toEqual(testBenches);
+  });
+
+  test("should handle RECEIVE_BENCH", () => {
+    let action = {
+      type: BenchActions.RECEIVE_BENCH,
+      bench: newBench
+    };
+
+    expect(BenchesReducer(testBenches, action)).toEqual(
+      Object.assign({}, testBenches, { 3: newBench })
+    );
+  });
+});
